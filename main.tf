c173a4b659fea400b96873febbd0b19f3d127fe3
Varun Sharma <varun@arintech.in>
2024-12-04 13:11:14 +0530

    End Of Lab: Launching VMs

--- main.tf
+++ main.tf
@@ -5,4 +5,13 @@
      key = "student.1-network-state"
      region = "ap-south-1"
   }
+ } 
+
+ resource "aws_instance" "my_vm" {
+  ami = "ami-053b12d3152c0cc71"
+ subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+ instance_type = "t2.micro"
+ tags = {
+     Name = "student.1-vm1"
+   }
  }
