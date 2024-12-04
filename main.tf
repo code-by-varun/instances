9cece4d35e031ec0dceb689ae7886f2bb890c469
Varun Sharma <varun@arintech.in>
2024-12-04 14:41:01 +0530

    ssh-added

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@
  resource "aws_instance" "my_vm" {
   ami = "ami-053b12d3152c0cc71"
  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+ key_name = data.terraform_remote_state.network_details.outputs.key_name
  instance_type = "t2.micro"
  tags = {
      Name = "student.1-vm1"
