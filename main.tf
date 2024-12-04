862d2aa239244ecdd0266d903047e69637e11fbb
Varun Sharma <varun@arintech.in>
2024-12-04 16:11:29 +0530

    update

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@
    config = {
      bucket = "student.1-varun-bucket"
      key = "student.1-network-state"
-     region = "ap-south-1"
+     region = var.region
   }
  } 
 
  resource "aws_instance" "my_vm" {
-  ami = "ami-053b12d3152c0cc71"
+  ami = var.ami
  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
  key_name = data.terraform_remote_state.network_details.outputs.key_name
  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
- instance_type = "t2.micro"
+ instance_type = var.instance
  tags = {
      Name = "student.1-vm1"
    }
