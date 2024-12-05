1a66e778091c53b18b7691b37ce290df25957cb5
Varun Sharma <varun@arintech.in>
2024-12-05 15:50:57 +0530

    update

--- modules/linux_node/instances.tf
+++ modules/linux_node/instances.tf
@@ -2,11 +2,22 @@ resource "aws_instance" "my_vm" {
   count                  = var.instance_count
   ami                    = var.ami
   subnet_id              = var.subnet_id
+  instance_type          = var.instance
   key_name               = var.key_name
   vpc_security_group_ids = var.vpc_security_group_ids
-  instance_type          = var.instance
-  tags = {
-    Name = "var.tags"
+  tags                   = var.tags
+
+
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      type        = "ssh"   
+      user        = "ubuntu"
+ #     agent       = false
+      private_key = file("/home/varun/terrafrom_base/keys/student.1-vm.key")
+    }
+    inline = [
+      "sudo apt install -y python3"
+    ]
   }
 }
-
